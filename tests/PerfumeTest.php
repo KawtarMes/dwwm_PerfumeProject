5e039942c67fab52f4e3e83d6c7b5c435b88eb7b
KawtarMes <mesfaouyi.kawtar@outlook.fr>
2024-04-25 09:28:57 +0200

    test unitaire correct sur entité Perfume, avec la ligne sur la var database en phpunit.xml

---
+++ tests/PerfumeTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Tests;
+
+use App\Entity\Perfume;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+class PerfumeTest extends KernelTestCase
+{
+    public function testPerfumeEntity(): void
+    {
+        $kernel = self::bootKernel();
+        $container = static::getContainer();
+
+        $perfume = new Perfume();
+
+        $perfume->setPerfumeTitle('PerfumTestTitle')
+            ->setDescription('DescriptionTest')
+            ->setOlfactiveFamilyId(null)
+            ->setPrice(100)
+            ->setBrand('BrandTest')
+            ->setQuantity(1)
+            ->setVolume(50)
+            ->setGender('femme');
+
+        $errors = $container->get('validator')->validate($perfume);
+
+        $this->assertCount(0, $errors);
+        $this->assertSame('PerfumTestTitle', $perfume->getPerfumeTitle());
+        $this->assertSame('DescriptionTest', $perfume->getDescription());
+
+
+
+
+        // $this->assertSame('test', $kernel->getEnvironment());
+        // // $routerService = static::getContainer()->get('router');
+        // // $myCustomService = static::getContainer()->get(CustomService::class);
+    }
+}
